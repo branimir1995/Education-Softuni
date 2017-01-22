b8b07ca185fac8763126b16db1393192b99d24ed
Branimir <branimir_1995@abv.bg>
2017-01-22 21:16:24 +0200

    new projects

---
+++ SoftUni/TechModule/Programming Fundamentals/Labs/DataTypesAndVariables/Elevator/Elevator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Elevator
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int weight = int.Parse(Console.ReadLine());
+
+            int courses = people / weight;
+
+            if (people % weight == 0)
+            {
+                
+            }
+            else
+            {
+                courses += 1;
+            }
+
+            Console.WriteLine(courses);
+
+
+        }
+    }
+}
---
+++ SoftUni/TechModule/Programming Fundamentals/Labs/DataTypesAndVariables/TriplesOfLetters/TriplesOfLetters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriplesOfLetters
+{
+    class TriplesOfLetters
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    for (int thirdletter = 0; thirdletter < num; thirdletter++)
+                    {
+                        char letterOne = (char)('a' + i);
+                        char letterTwo = (char)('a' + j);
+                        char letterThree = (char)('a' + thirdletter);
+
+                        Console.WriteLine($"{letterOne}{letterTwo}{letterThree}");
+                    }
+                   
+                }
+               
+            }
+
+
+        }
+    }
+}

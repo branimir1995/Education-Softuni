6ae293c4461b3ac8dd1a52784cda84e77cb1ae00
Branimir <branimir_1995@abv.bg>
2017-01-24 17:08:21 +0200

    Couple of new programs

---
+++ SoftUni/TechModule/Programming Fundamentals/Exercises/DataTypesAndVariablesExcercise/CenturiesToNanosecond/CenturiesToNanoseconds.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace DataTypesAndVariables
+{
+    public class CenturiesToMinutes
+    {
+        public static void Main()
+        {
+
+            int centuries = int.Parse(Console.ReadLine());
+            int years = 100 * centuries;
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+            long seconds = minutes * 60;
+            long nanoSeconds = seconds * 1000000000;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds");
+
+
+        }
+    }
+}

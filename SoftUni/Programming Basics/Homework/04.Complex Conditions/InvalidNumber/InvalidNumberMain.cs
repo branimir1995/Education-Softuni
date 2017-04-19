9ba064b21dd383b0e5d3c3d89f1c08f3a7a73e74
branimir1995 <branimir_1995@abv.bg>
2017-04-19 15:03:31 +0300

    added complex conditions homework

---
+++ SoftUni/Programming Basics/Homework/04.Complex Conditions/InvalidNumber/InvalidNumberMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvalidNumber
+{
+    class InvalidNumberMain
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            if (!(a >= 100 && a <= 200 || a == 0))
+            {
+                Console.WriteLine("invalid");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
